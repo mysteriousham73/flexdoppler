fbae68b5f1013f4be51a3a993c7652f5ddc8ee8f
mysteriousham <mysteriousham73@gmail.com>
2020-01-12 18:10:55 -0600

    fixing travis ci builds

--- travis/install_python.sh
+++ travis/install_python.sh
@@ -21,7 +21,7 @@ echo "------ Add latest wine repo ------"
 #bash travis/travis_retry.sh sudo dpkg --add-architecture i386
 wget -nc https://dl.winehq.org/wine-builds/winehq.key
 bash travis/travis_retry.sh sudo apt-key add winehq.key
-bash travis/travis_retry.sh sudo apt-add-repository 'deb https://dl.winehq.org/wine-builds/ubuntu/ bionic main'
+bash travis/travis_retry.sh sudo apt-add-repository 'deb https://dl.winehq.org/wine-builds/ubuntu/ xenial main'
 bash travis/travis_retry.sh sudo apt update
 
 # Add repo for faudio package.  Required for winedev

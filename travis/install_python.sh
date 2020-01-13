fc2a45a43ef7623da26d75cefc9b6c1f5304c5e9
mysteriousham <mysteriousham73@gmail.com>
2020-01-12 19:19:19 -0600

    fixing travis ci builds

--- travis/install_python.sh
+++ travis/install_python.sh
@@ -33,7 +33,8 @@ WINEPREFIX=~/.wine64 WINARCH=win64 winetricks corefonts
 
 echo "------ Install python ------"
 #export DISPLAY=:0.0
-DISPLAY=:99.0 WINEPREFIX=~/.wine64 wine cmd /c python-3.6.8-amd64.exe /quiet PrependPath=1
+export WINEPREFIX=~/.wine64
+xvfb-run --server-args="-screen 0 1024x768x24" wine cmd /c python-3.6.8-amd64.exe /quiet PrependPath=1
 echo "Python Installation complete!"
 # Display=:0.0 redirects wine graphical output to the dummy display.
 # This is to avoid docker errors as the python installer requires a display,

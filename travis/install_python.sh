3ccd6945893f1f864a766c3bd0ec95d8c2aea07c
mysteriousham <mysteriousham73@gmail.com>
2020-01-12 18:39:07 -0600

    fixing travis ci builds

--- travis/install_python.sh
+++ travis/install_python.sh
@@ -46,11 +46,9 @@ sudo Xvfb :0 -screen 0 1024x768x16 &
 jid=$!
 
 echo "------ Install python ------"
-DISPLAY=:0.0 WINEPREFIX=~/.wine64 wine cmd /c \
-    python-3.6.8-amd64.exe \
-    /quiet \
-    PrependPath=1 \
-    && echo "Python Installation complete!"
+export DISPLAY=:0.0
+WINEPREFIX=~/.wine64 wine cmd /c python-3.6.8-amd64.exe /quiet PrependPath=1
+echo "Python Installation complete!"
 # Display=:0.0 redirects wine graphical output to the dummy display.
 # This is to avoid docker errors as the python installer requires a display,
 # even when quiet install is specified.
